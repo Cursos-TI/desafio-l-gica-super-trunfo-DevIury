dee341e08d2bff6933d6deef9e270f733eb70f58
Iury Gabriel <168091111+DevIury@users.noreply.github.com>
2025-01-30 12:14:30 +0000

    Usei o outro código e adicionei as novidades

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,19 +8,59 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    int codigo, pontos, codigo2, pontos2;
+    char cidade[50], cidade2[50];
+    float povo, area, povo2, area2;
+    double PIB, PIB2;
 
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%d", &codigo);
+
+    printf("Digite a população: \n");
+    scanf("%f", &povo);
+
+    printf("Cite quantos pontos turísticos a cidade possui: \n");
+    scanf("%d", &pontos);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%lf", &PIB);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade2);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%d", &codigo2);
+
+    printf("Digite a população: \n");
+    scanf("%f", &povo2);
+
+    printf("Cite quantos pontos turísticos a cidade possui: \n");
+    scanf("%d", &pontos2);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB: \n");
+    scanf("%lf", &PIB2);
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    if(povo > povo2){
+        printf("Cidade 1 tem maior população que cidade 2!");
+    }else{
+        printf("Cidade 2 tem maior população que a cidade 1!");
+    }
 
+    if(pontos > pontos2){
+        
+    }
+    
+    
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
